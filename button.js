6c66bfde6c72c629753da37c0bc7e31299fe6e97
halilovilsur26-code <halilovilsur26@gmail.com>
2026-01-16 05:04:11 +0500

    Add files via upload

---
+++ button.js
@@ -0,0 +1,12 @@
+// ВСЕ кнопки
+document.querySelectorAll('.watchBtn').forEach(btn => {
+    const id = btn.dataset.id;
+    
+    btn.textContent = localStorage.getItem(id) === 'true' ? '🤍' : '❤️';
+
+    btn.onclick = () => {
+        const willBeWhite = btn.textContent === '❤️';
+        btn.textContent = willBeWhite ? '🤍' : '❤️';
+        localStorage.setItem(id, willBeWhite ? 'true' : 'false');
+    };
+});
